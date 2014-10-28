a62a6426d60e2c703caabd80ffd346e490c22d85
Sleeztack <Chris.Richardson@gordon.edu>
2014-10-27 21:30:40 -0400

    Chris Richardson

--- Midterm.py
+++ Midterm.py
@@ -30,19 +30,25 @@ Anavel=Exhaust_vel*np.log((Empty_mass+Max_fuel)/Empty_mass)
 
 dt=000000000001
 t=np.linspace(1,burn_time-dt, 10000)
-
-
 Ft_thrust=F_thrust*np.linspace(1,1,10000)
 VAna=Exhaust_vel*np.log((Empty_mass+Max_fuel)/(Empty_mass-(burn_rate*t)))
-velT=delMom/(delmass*t)
+velT=Ft_thrust/delmass*burn_time
 ypost=VAna*t
+
 # Gravity
 gs=9.81
 Rade=6.37*10**6
 gt=gs/(1+ypost/Rade)**2
-Anavel2=VAna-gs*t
+Vana2=VAna-gs*t
 # Air resistance
-
+Temp_Lapse = 0.0065
+Press_Sea = 1.01 * 10 ** 5
+Molar_mass = 0.02896
+Ideal_Gas = 8.314
+Sea_press=1.01*10**5
+T=288-ypost*Temp_Lapse
+Pressure=Sea_press*(T/288.0)**(gt*Molar_mass/(Ideal_Gas* Temp_Lapse))
+Forced=.5*Pressure*Area*drag_co*(Vana2)**2
 
 
 # Plots for position, velocity(Analytical and not), and force with respect to time
@@ -50,6 +56,7 @@ Anavel2=VAna-gs*t
 #Force
 
 plt.subplot(3,1,1)
+plt.plot(t,Forced,"g")
 plt.plot(t,Ft_thrust,"b")
 plt.plot(t,gt,"r")
 plt.title("Force [N] vs. Time [s]")
@@ -59,7 +66,7 @@ plt.ylabel("N")
 plt.subplot(3,1,2)
 plt.plot(t,velT,"b")
 plt.plot(t,VAna, "r")
-plt.plot(t,Anavel2,"k")
+plt.plot(t,Vana2,"k")
 plt.title("Velocity [m/s] vs. Time [s]")
 plt.ylabel("[m/s]")
 
