551439fe50561255d75dc3605a3b7f91c5682992
DanielMrt <daniel.martins@gordon.edu>
2014-10-27 21:34:02 -0500

    11111
    1123

--- Midterm.py
+++ Midterm.py
@@ -17,45 +17,39 @@ drag_co=.5
 
 ##Finding The forces using the above
 # These come from values regarding thrust
-delmass=Empty_mass
-
+delmass=Max_fuel
 burn_time=Max_fuel/burn_rate #aka delta t
 delMom=burn_rate*burn_time*Exhaust_vel #AKA delta P
 F_thrust=delMom/burn_time
-
 Anavel=Exhaust_vel*np.log((Empty_mass+Max_fuel)/Empty_mass)
-<<<<<<< HEAD
 print Anavel
 Positiony=Anavel*burn_time
-=======
-
->>>>>>> origin/master
+print Anavel
+Positiony=Anavel*burn_time
 
 # Graph equations
 
 dt=000000000001
-t=np.linspace(1,burn_time-dt, 10000)
-
-
+t=np.linspace(0,burn_time-dt, 10000)
+ypost=Anavel*t
 Ft_thrust=F_thrust*np.linspace(1,1,10000)
 VAna=Exhaust_vel*np.log((Empty_mass+Max_fuel)/(Empty_mass-(burn_rate*t)))
-velT=delMom/(delmass*t)
-ypost=VAna*t
+velT=delMom/delmass*t
+
 # Gravity
-gs=9.81
+gs=9.8
 Rade=6.37*10**6
+g=gs/(1+(Positiony/Rade))**2
+print g
 gt=gs/(1+ypost/Rade)**2
-<<<<<<< HEAD
+
 VAna2=VAna-gs*t
 
 #Air Resistence
-=======
+
 Anavel2=VAna-gs*t
 # Air resistance
-
->>>>>>> origin/master
-
-
+VAna2=VAna-gs*t
 # Plots for position, velocity(Analytical and not), and force with respect to time
 
 #Force
@@ -70,18 +64,12 @@ plt.ylabel("N")
 plt.subplot(3,1,2)
 plt.plot(t,velT,"b")
 plt.plot(t,VAna, "r")
-plt.plot(t,Anavel2,"k")
+plt.plot(t,VAna2,"k")
 plt.title("Velocity [m/s] vs. Time [s]")
 plt.ylabel("[m/s]")
 
 #position
-
-<<<<<<< HEAD
 print Positiony
-=======
-
-
->>>>>>> origin/master
 plt.subplot(3,1,3)
 plt.plot(t,ypost,"y")
 plt.title("Height [m/s] vs Time [s]")
